e002efa95c35c571e7bb7fc43277be475cdc26ec
PradermN <pradermnafack@gmail.com>
2024-11-27 15:34:26 +0100

    ajout de plantList et utilisation de reduce pour selectionner toutes les categories de la plantList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,28 +1,31 @@
 import React from "react";
 
+import { plantList } from "../datas/plantList";
 
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+
 
 export default function ShoppingList(){
 
+    const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
     return(
 
-        <ul>
-
-            {plantList.map((plant, index) => (
-
-            <li key={index}>{ plant }</li>
-
-
-            ))}
-
-        </ul>
+        <div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul>
+				{plantList.map((plant) => (
+					<li key={plant.id}>{plant.name}</li>
+				))}
+			</ul>
+		</div>
 
 
     )
