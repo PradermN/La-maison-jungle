9c8fe2a68a3aa9945ed841cd3e79cb974ab1029e
PradermN <pradermnafack@gmail.com>
2024-11-27 19:48:22 +0100

    Ajout de handleInput et handleBlur au Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,16 @@ export default function Footer(){
 
     const [inputValue, setInputValue] = useState('')
 
+    function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+    function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
+
     return(
 
         <footer className='lmj-footer'>
@@ -15,6 +25,12 @@ export default function Footer(){
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+            <input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
 		</footer>
     )
 }
