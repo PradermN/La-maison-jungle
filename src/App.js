b5361cd29b9a9cf87990b6f4b5192396aeba88a7
PradermN <pradermnafack@gmail.com>
2024-12-01 20:35:31 +0100

    Ajout d'un useEffect dans Banner pour stocker le prix total dans le local storage

--- src/App.js
+++ src/App.js
@@ -3,25 +3,35 @@ import Cart from './components/Cart';
 import Footer from './components/Footer';
 import ShoppingList from './components/ShoppingList';
 import '../src/styles/Layout.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import logo from './assets/logo.png'
 
 function App() {
-  // Ajout du state cart au parent et cart est initialisé comme un tableau
-  const [cart, updateCart] = useState([])
+
+	const savedCart = localStorage.getItem('cart')
+
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
+
 		<div>
+
 			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+
 			<div className='lmj-layout-inner'>
 				<Cart cart={cart} updateCart={updateCart} />
 				<ShoppingList cart={cart} updateCart={updateCart} />
 			</div>
+      
 			<Footer />
 		</div>
-	);
+	)
 }
 
 export default App;
